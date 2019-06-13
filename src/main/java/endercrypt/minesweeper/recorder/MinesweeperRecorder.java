c571b755b831d66e150eff5a07891ddea11133f6
EnderCrypt <endercrypt@gmail.com>
2019-06-13 05:40:57 +0200

    added ability to save frames

--- src/main/java/endercrypt/minesweeper/recorder/MinesweeperRecorder.java
+++ src/main/java/endercrypt/minesweeper/recorder/MinesweeperRecorder.java
@@ -1,11 +1,20 @@
 package endercrypt.minesweeper.recorder;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import endercrypt.minesweeper.Minesweeper;
 import endercrypt.minesweeper.MinesweeperChild;
 
 public class MinesweeperRecorder extends MinesweeperChild
 {
+	private int width = -1;
+	private int height = -1;
+
 	private boolean recording = false;
+	private List<RecordingFrame> frames = new ArrayList<>();
 
 	public MinesweeperRecorder(Minesweeper minesweeper)
 	{
@@ -24,6 +33,24 @@ public class MinesweeperRecorder extends MinesweeperChild
 
 	public void saveFrame(RecordingFrame frame)
 	{
-		// TODO Auto-generated method stub
+		Objects.requireNonNull(frame);
+		if (this.frames.size() == 0)
+		{
+			this.width = frame.getWidth();
+			this.height = frame.getHeight();
+		}
+		else
+		{
+			if (frame.getWidth() != this.width || frame.getHeight() != this.height)
+			{
+				throw new IllegalArgumentException("frame width,height (" + frame.getWidth() + ", " + frame.getHeight() + ") does not match with recording width,height (" + this.width + ", " + this.height + ")");
+			}
+		}
+		this.frames.add(frame);
+	}
+
+	public void saveGif(Path path)
+	{
+
 	}
 }
