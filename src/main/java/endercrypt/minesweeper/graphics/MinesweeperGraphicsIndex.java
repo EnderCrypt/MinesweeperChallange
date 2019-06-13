933814474dcdf4c6078d1072ded464d517ae1816
EnderCrypt <endercrypt@gmail.com>
2019-06-13 11:18:33 +0200

    fixed graphics start position

--- src/main/java/endercrypt/minesweeper/graphics/MinesweeperGraphicsIndex.java
+++ src/main/java/endercrypt/minesweeper/graphics/MinesweeperGraphicsIndex.java
@@ -4,7 +4,7 @@ public class MinesweeperGraphicsIndex
 {
 	public static final MinesweeperGraphicsRange NUMBERS = new MinesweeperGraphicsRange(0, 0, 8, (i) -> " " + i + " ");
 
-	private static int index = 9;
+	private static int index = 8;
 	public static final MinesweeperGraphicsPointer EMPTY = new MinesweeperGraphicsPointer(index++, "   ");
 	public static final MinesweeperGraphicsPointer MINE = new MinesweeperGraphicsPointer(index++, " X ");
 	public static final MinesweeperGraphicsPointer UNOPENED = new MinesweeperGraphicsPointer(index++, "[ ]");
